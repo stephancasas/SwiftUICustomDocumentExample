3365a40aa6e53ad5ce9dc36c2f0363634d52102e
Stephan Casas <stephancasas@icloud.com>
2023-06-24 11:13:11 -0500

    Initial Commit

---
+++ SwiftUICustomDocumentExample/SwiftUICustomDocumentExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomDocumentExampleApp.swift
+//  SwiftUICustomDocumentExample
+//
+//  Created by Stephan Casas on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomDocumentExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
